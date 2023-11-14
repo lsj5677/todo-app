bea2d5a46373b73b8cfbd45cd5ac42fe9b8d8c7b
lsj5677 <lsj5677@naver.com>
2023-11-13 22:52:36 -0500

    Create front/backend

---
+++ backend/src/routes/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import todoRoutes from "./todo.routes.js";
+
+const router = express.Router();
+router.use("/todos", todoRoutes);
+
+export default router;
---
+++ backend/src/routes/todo.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  getTodos,
+} from "../controller/todo.controllers.js";
+
+const router = express.Router();
+
+// add a todo
+router.post("/", addTodo);
+// get todos
+router.get("/", getTodos);
+// update a todo
+router.put("/:id", updateTodo);
+// remove a todo
+router.delete("/:id", deleteTodo);
+
+export default router;
